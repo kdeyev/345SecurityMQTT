c9d41c0e90d7367ee5c8f7912f3e774566eb5158
Kostya Deev <kostya.deev@bluware.com>
2021-12-29 23:43:20 -0600

    add device_id

--- run.sh
+++ run.sh
@@ -6,12 +6,14 @@ if bashio::services.available "mqtt"; then
     MQTT_PORT=$(bashio::services "mqtt" "port")
     MQTT_USERNAME=$(bashio::services "mqtt" "username")
     SEND_DISCOVERY=$(bashio::config "discovery")
+    DEVICE_ID=$(bashio::config "device_id")
+    FREQUENCY=$(bashio::config "frequency")
 else
     bashio::log.info "The mqtt addon is not available."
     bashio::log.info "Manually update the output line in the configuration file with mqtt connection settings, and restart the addon."
 fi
 
-echo "Starting 345SecurityMQTT"
-./345toMqtt
+echo "Starting 345toMqtt -d $DEVICE_ID -f $FREQUENCY"
+./345toMqtt -d $DEVICE_ID -f $FREQUENCY
 echo "Exiting run.sh"
 exit $?
