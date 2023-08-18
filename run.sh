813178c7f012bbc9077654377489e371f7029d0a
Konstantin Deev <kn.deev@gmail.com>
2023-08-17 19:32:54 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,17 +11,28 @@
 #     exit $?
 # fi
 
-AUTODISCOVERY=$(bashio::config "autodiscovery")
-DEVICE_ID=$(bashio::config "device_id")
-FREQUENCY=$(bashio::config "frequency")
-SAMPLE_RATE=$(bashio::config "sample_rate")
-GAIN=$(bashio::config "gain")
+# AUTODISCOVERY=$(bashio::config "autodiscovery")
+# DEVICE_ID=$(bashio::config "device_id")
+# FREQUENCY=$(bashio::config "frequency")
+# SAMPLE_RATE=$(bashio::config "sample_rate")
+# GAIN=$(bashio::config "gain")
 
-export MQTT_HOST=$(bashio::config "mqtt_host")
-export MQTT_PORT=$(bashio::config "mqtt_port")
-export MQTT_USERNAME=$(bashio::config "mqtt_username")
-export MQTT_PASSWORD=$(bashio::config "mqtt_password")
-    
+# export MQTT_HOST=$(bashio::config "mqtt_host")
+# export MQTT_PORT=$(bashio::config "mqtt_port")
+# export MQTT_USERNAME=$(bashio::config "mqtt_username")
+# export MQTT_PASSWORD=$(bashio::config "mqtt_password")
+
+AUTODISCOVERY=false
+DEVICE_ID=1
+FREQUENCY=345000000
+SAMPLE_RATE=1000000
+GAIN=700
+
+export MQTT_HOST=localhost
+export MQTT_PORT=1883
+export MQTT_USERNAME=mqtt
+export MQTT_PASSWORD=mqtt
+
 echo "Starting 345toMqtt -d $DEVICE_ID -f $FREQUENCY -a $AUTODISCOVERY -g $GAIN -s $SAMPLE_RATE"
 echo "$MQTT_HOST $MQTT_PORT $MQTT_USERNAME $MQTT_PASSWORD"
 ./345toMqtt -d $DEVICE_ID -f $FREQUENCY -a $AUTODISCOVERY -g $GAIN -s $SAMPLE_RATE
