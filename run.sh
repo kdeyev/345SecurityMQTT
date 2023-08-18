45a0c654d9b01b1f5de311d1b0e894150ac5f64b
Konstantin Deev <kn.deev@gmail.com>
2023-08-17 19:18:55 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bashio
 
-if bashio::services.available "mqtt"; then
-    export MQTT_HOST=$(bashio::services "mqtt" "host")
-    export MQTT_PASSWORD=$(bashio::services "mqtt" "password")
-    export MQTT_PORT=$(bashio::services "mqtt" "port")
-    export MQTT_USERNAME=$(bashio::services "mqtt" "username")
-else
-    bashio::log.info "The mqtt addon is not available."
-    bashio::log.info "Manually update the output line in the configuration file with mqtt connection settings, and restart the addon."
-    exit $?
-fi
+# if bashio::services.available "mqtt"; then
+#     export MQTT_HOST=$(bashio::services "mqtt" "host")
+#     export MQTT_PASSWORD=$(bashio::services "mqtt" "password")
+#     export MQTT_PORT=$(bashio::services "mqtt" "port")
+#     export MQTT_USERNAME=$(bashio::services "mqtt" "username")
+# else
+#     bashio::log.info "The mqtt addon is not available."
+#     bashio::log.info "Manually update the output line in the configuration file with mqtt connection settings, and restart the addon."
+#     exit $?
+# fi
 
 AUTODISCOVERY=$(bashio::config "autodiscovery")
 DEVICE_ID=$(bashio::config "device_id")
@@ -17,6 +17,11 @@ FREQUENCY=$(bashio::config "frequency")
 SAMPLE_RATE=$(bashio::config "sample_rate")
 GAIN=$(bashio::config "gain")
 
+export MQTT_HOST=$(bashio::config "mqtt_host")
+export MQTT_PORT=$(bashio::config "mqtt_port")
+export MQTT_USERNAME=$(bashio::config "mqtt_username")
+export MQTT_PASSWORD=$(bashio::config "mqtt_password")
+    
 echo "Starting 345toMqtt -d $DEVICE_ID -f $FREQUENCY -a $AUTODISCOVERY -g $GAIN -s $SAMPLE_RATE"
 ./345toMqtt -d $DEVICE_ID -f $FREQUENCY -a $AUTODISCOVERY -g $GAIN -s $SAMPLE_RATE
 echo "Exiting run.sh"
